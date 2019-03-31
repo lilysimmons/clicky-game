2aca79277f5c49aa92a3ad223431ea5b0bcc93fc
Lily <44213621+lilysimmons@users.noreply.github.com>
2019-03-31 18:10:17 -0400

    shuffle works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import ParrotCard from "./components/ParrotCard";
+import ParrotCard from "./components/ParrotCard/parrot";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import parrots from "./parrots.json";
@@ -7,8 +7,62 @@ import parrots from "./parrots.json";
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    parrots
+    parrots,
+    clickedParrotIDs: [],
+    score: 0,
+    topScore: 12,
+    status: ""
+  }
+
+
+
+  shuffle = (id) => {
+    console.log("CAW CAW")
+    let clickedParrotIDs = this.state.clickedParrotIDs;
+
+    if (clickedParrotIDs.includes(id)) {
+      this.setState({
+        clickedParrotIDs: [],
+        score: 0,
+        status: "You lost loser!!!! Click on an image to try again"
+      })
+      return
+    } else {
+      clickedParrotIDs.push(id)
+    }
+
+    if (clickedParrotIDs === 12) {
+      this.setState({
+        score: 12,
+        status: "You won! Your memory is on fleek.",
+        clickedParrotIDs: []
+      });
+      alert("Play again?");
+      return
+    }
+
+    this.setState({ parrots, clickedParrotIDs, score: clickedParrotIDs.length, status: "" });
+
+    //fisher-yates shuffle
+    var i = 0, j = 0, temp = null
+
+    for (i = parrots.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = parrots[i]
+      parrots[i] = parrots[j]
+      parrots[j] = temp
+    }
+
+
   };
+
+
+
+  //for (let i = parrots.length - 1; i > 0; i--) {
+  //   let j = Math.floor(Math.random() * (i + 1));
+  //   [parrots[i], parrots[j]] = [parrots[j], parrots[i]];
+  // };
+
 
   Score = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
@@ -21,16 +75,20 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Title>Parrot List</Title>
+        {/* <Header>
+          <h1>P</h1>
+        </Header> */}
+        <Title>Clicky Parrot Shuffle</Title>
         {this.state.parrots.map(parrot => (
           <ParrotCard
             // removeFriend={this.removeParrot}
+            shuffle={this.shuffle}
             id={parrot.id}
             key={parrot.id}
-            name={parrot.name}
+            // name={parrot.name}
             image={parrot.image}
-            // occupation={parrot.occupation}
-            // location={parrot.location}
+          // occupation={parrot.occupation}
+          // location={parrot.location}
           />
         ))}
       </Wrapper>
