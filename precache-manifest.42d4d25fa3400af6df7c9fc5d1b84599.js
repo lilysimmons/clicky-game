885dcbcc8aacd8e4215790957474bc33625b7f0f
Lily <44213621+lilysimmons@users.noreply.github.com>
2019-04-02 21:37:45 -0400

    Updates

---
+++ precache-manifest.42d4d25fa3400af6df7c9fc5d1b84599.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "eaf96238eada18e6fa16",
+    "url": "/clicky-game/static/js/main.5ffd44ad.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "eaf96238eada18e6fa16",
+    "url": "/clicky-game/static/css/main.144d7dec.chunk.css"
+  },
+  {
+    "revision": "375f4ac1841c6e119ef448eb5d26d25d",
+    "url": "/clicky-game/index.html"
+  }
+];
